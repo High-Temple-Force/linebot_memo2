5d081ae887fce1fb6bb212b8dc4c6d5ce66f1632
bluesDD <dd11blues@gmail.com>
2018-11-21 12:16:36 +0900

    connection if

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const client_db = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
 });
+
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
@@ -27,6 +28,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
         .catch((err) => {
             console.error(err);
             res.status(500).end();
+            return;
         });
     const userid = req.body.events.map(getid);
     const message_text = req.body.events.map(getmessage);
@@ -40,8 +42,8 @@ app.post('/callback', line.middleware(config), (req, res) => {
         console.log('connected to POSTGRE');
     });
     const query_bot = `INSERT into linebot_message VALUES 
-    (1, '${userid[0]}', '${message_text[0]}') ON CONFLICT (user_id) 
-    DO UPDATE set text = '${message_text[0]}' where user_id = '${userid[0]}';`;
+    (1, ${userid[0]}, ${message_text[0]}) ON CONFLICT (user_id) 
+    DO UPDATE set text = ${message_text[0]} where user_id = ${userid[0]};`;
     client_db.query(query_bot, function(err, result) {
             if(err) return console.error(err);
         });
